51ee5b34aa3b19cf959c7ff32c731a0ab5a2e013
Matt Love <mlove@parasoft.com>
2016-11-23 09:48:04 -0800

    initial Docker files for Parasoft CTP and SOAVirt

---
+++ ctp/startCTP.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# Evaluate and replace the environment variables in the Virtualize config
+
+sed -i "s/\$VIRTUALIZE_SERVER_NAME/$VIRTUALIZE_SERVER_NAME/g" $SOAVIRT_HOME/WEB-INF/config.properties
+sed -i "s/\$CTP_HOST/$CTP_HOST/g" $SOAVIRT_HOME/WEB-INF/config.properties
+sed -i "s/\$CTP_PORT/$CTP_PORT/g" $SOAVIRT_HOME/WEB-INF/config.properties
+sed -i "s/\$CTP_USERNAME/$CTP_USERNAME/g" $SOAVIRT_HOME/WEB-INF/config.properties
+sed -i "s/\$CTP_PASSWORD/$CTP_PASSWORD/g" $SOAVIRT_HOME/WEB-INF/config.properties
+sed -i "s/\$CTP_NOTIFY/$CTP_NOTIFY/g" $SOAVIRT_HOME/WEB-INF/config.properties
+
+sed -i "s/\$LICENSE_EDITION/$LICENSE_EDITION/g" $SOAVIRT_HOME/WEB-INF/config.properties
+sed -i "s/\$LICENSE_FEATURES/$LICENSE_FEATURES/g" $SOAVIRT_HOME/WEB-INF/config.properties
+sed -i "s/\$LICENSE_SERVER_HOST/$LICENSE_SERVER_HOST/g" $SOAVIRT_HOME/WEB-INF/config.properties
+sed -i "s/\$LICENSE_SERVER_PORT/$LICENSE_SERVER_PORT/g" $SOAVIRT_HOME/WEB-INF/config.properties
+
+sed -i "s/\$LICENSE_SERVER_HOST/$LICENSE_SERVER_HOST/g" $CATALINA_HOME/ctp/webapps/em/license
+sed -i "s/\$LICENSE_SERVER_PORT/$LICENSE_SERVER_PORT/g" $CATALINA_HOME/ctp/webapps/em/license
+
+echo "Starting the Continuous Testing Platform..."
+mkdir -p $SOAVIRT_HOME/workspace/VirtualAssets/logs/ctp
+export CATALINA_BASE=$CATALINA_HOME/ctp
+nohup $CATALINA_HOME/bin/catalina.sh run >> $SOAVIRT_HOME/workspace/VirtualAssets/logs/ctp/catalina.log 2>&1 &
+sleep 5
+tail $SOAVIRT_HOME/workspace/VirtualAssets/logs/ctp/catalina.log
+sleep 25 # Wait for CTP to start up so TDM and Virtualize can register
+tail $SOAVIRT_HOME/workspace/VirtualAssets/logs/ctp/catalina.log
+
+# Evaluate and replace the environment variables in the Data Repository Server
+if [ -f $DATA_REPOSITORY_HOME/server.sh ]
+then
+	sed -i "s/\$CTP_HOST/$CTP_HOST/g" $DATA_REPOSITORY_HOME/server.sh
+	sed -i "s/\$CTP_PORT/$CTP_PORT/g" $DATA_REPOSITORY_HOME/server.sh
+	sed -i "s/\$CTP_USERNAME/$CTP_USERNAME/g" $DATA_REPOSITORY_HOME/server.sh
+	sed -i "s/\$CTP_PASSWORD/$CTP_PASSWORD/g" $DATA_REPOSITORY_HOME/server.sh
+	$DATA_REPOSITORY_HOME/server.sh start
+fi
+
+echo "Starting the SOAVirt server..."
+mkdir -p $SOAVIRT_HOME/workspace/VirtualAssets/logs/virtualize
+export CATALINA_BASE=$CATALINA_HOME/soavirt
+nohup $CATALINA_HOME/bin/catalina.sh run >> $SOAVIRT_HOME/workspace/VirtualAssets/logs/virtualize/catalina.log 2>&1 &
+
+tail -f $SOAVIRT_HOME/workspace/VirtualAssets/logs/virtualize/catalina.log
---
+++ soavirt/startSOAVirtServer.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Evaluate and replace the environment variables in the Data Repository Server
+if [ -f $DATA_REPOSITORY_HOME/server.sh ]
+then
+	sed -i "s/\$CTP_HOST/$CTP_HOST/g" $DATA_REPOSITORY_HOME/server.sh
+	sed -i "s/\$CTP_PORT/$CTP_PORT/g" $DATA_REPOSITORY_HOME/server.sh
+	sed -i "s/\$CTP_USERNAME/$CTP_USERNAME/g" $DATA_REPOSITORY_HOME/server.sh
+	sed -i "s/\$CTP_PASSWORD/$CTP_PASSWORD/g" $DATA_REPOSITORY_HOME/server.sh
+	$DATA_REPOSITORY_HOME/server.sh start
+fi
+
+# Evaluate and replace the environment variables in the Virtualize config
+
+sed -i "s/\$VIRTUALIZE_SERVER_NAME/$VIRTUALIZE_SERVER_NAME/g" $SOAVIRT_HOME/WEB-INF/config.properties
+sed -i "s/\$CTP_HOST/$CTP_HOST/g" $SOAVIRT_HOME/WEB-INF/config.properties
+sed -i "s/\$CTP_PORT/$CTP_PORT/g" $SOAVIRT_HOME/WEB-INF/config.properties
+sed -i "s/\$CTP_USERNAME/$CTP_USERNAME/g" $SOAVIRT_HOME/WEB-INF/config.properties
+sed -i "s/\$CTP_PASSWORD/$CTP_PASSWORD/g" $SOAVIRT_HOME/WEB-INF/config.properties
+sed -i "s/\$CTP_NOTIFY/$CTP_NOTIFY/g" $SOAVIRT_HOME/WEB-INF/config.properties
+
+sed -i "s/\$LICENSE_EDITION/$LICENSE_EDITION/g" $SOAVIRT_HOME/WEB-INF/config.properties
+sed -i "s/\$LICENSE_FEATURES/$LICENSE_FEATURES/g" $SOAVIRT_HOME/WEB-INF/config.properties
+sed -i "s/\$LICENSE_SERVER_HOST/$LICENSE_SERVER_HOST/g" $SOAVIRT_HOME/WEB-INF/config.properties
+sed -i "s/\$LICENSE_SERVER_PORT/$LICENSE_SERVER_PORT/g" $SOAVIRT_HOME/WEB-INF/config.properties
+
+export CATALINA_BASE=$CATALINA_HOME/soavirt
+$CATALINA_HOME/bin/catalina.sh run
