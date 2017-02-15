169181efd60e9d162396dfcf1ce8e0318bfa56dc
Matt Love <mlove@parasoft.com>
2017-02-15 15:57:55 -0800

    changes to allow CTP to startup without any SOAVirt

--- ctp/startCTP.sh
+++ ctp/startCTP.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 # Evaluate and replace the environment variables in the Virtualize config
-
-sed -i "s/\$VIRTUALIZE_SERVER_NAME/$VIRTUALIZE_SERVER_NAME/g" $SOAVIRT_HOME/WEB-INF/config.properties
-sed -i "s/\$CTP_HOST/$CTP_HOST/g" $SOAVIRT_HOME/WEB-INF/config.properties
-sed -i "s/\$CTP_PORT/$CTP_PORT/g" $SOAVIRT_HOME/WEB-INF/config.properties
-sed -i "s/\$CTP_USERNAME/$CTP_USERNAME/g" $SOAVIRT_HOME/WEB-INF/config.properties
-sed -i "s/\$CTP_PASSWORD/$CTP_PASSWORD/g" $SOAVIRT_HOME/WEB-INF/config.properties
-sed -i "s/\$CTP_NOTIFY/$CTP_NOTIFY/g" $SOAVIRT_HOME/WEB-INF/config.properties
-
-sed -i "s/\$LICENSE_EDITION/$LICENSE_EDITION/g" $SOAVIRT_HOME/WEB-INF/config.properties
-sed -i "s/\$LICENSE_FEATURES/$LICENSE_FEATURES/g" $SOAVIRT_HOME/WEB-INF/config.properties
-sed -i "s/\$LICENSE_SERVER_HOST/$LICENSE_SERVER_HOST/g" $SOAVIRT_HOME/WEB-INF/config.properties
-sed -i "s/\$LICENSE_SERVER_PORT/$LICENSE_SERVER_PORT/g" $SOAVIRT_HOME/WEB-INF/config.properties
-
-sed -i "s/\$LICENSE_SERVER_HOST/$LICENSE_SERVER_HOST/g" $CATALINA_HOME/ctp/webapps/em/license
-sed -i "s/\$LICENSE_SERVER_PORT/$LICENSE_SERVER_PORT/g" $CATALINA_HOME/ctp/webapps/em/license
+if [ -f $SOAVIRT_HOME/WEB-INF/config.properties ]
+then
+	sed -i "s/\$VIRTUALIZE_SERVER_NAME/$VIRTUALIZE_SERVER_NAME/g" $SOAVIRT_HOME/WEB-INF/config.properties
+	sed -i "s/\$CTP_HOST/$CTP_HOST/g" $SOAVIRT_HOME/WEB-INF/config.properties
+	sed -i "s/\$CTP_PORT/$CTP_PORT/g" $SOAVIRT_HOME/WEB-INF/config.properties
+	sed -i "s/\$CTP_USERNAME/$CTP_USERNAME/g" $SOAVIRT_HOME/WEB-INF/config.properties
+	sed -i "s/\$CTP_PASSWORD/$CTP_PASSWORD/g" $SOAVIRT_HOME/WEB-INF/config.properties
+	sed -i "s/\$CTP_NOTIFY/$CTP_NOTIFY/g" $SOAVIRT_HOME/WEB-INF/config.properties
+	sed -i "s/\$LICENSE_EDITION/$LICENSE_EDITION/g" $SOAVIRT_HOME/WEB-INF/config.properties
+	sed -i "s/\$LICENSE_FEATURES/$LICENSE_FEATURES/g" $SOAVIRT_HOME/WEB-INF/config.properties
+	sed -i "s/\$LICENSE_SERVER_HOST/$LICENSE_SERVER_HOST/g" $SOAVIRT_HOME/WEB-INF/config.properties
+	sed -i "s/\$LICENSE_SERVER_PORT/$LICENSE_SERVER_PORT/g" $SOAVIRT_HOME/WEB-INF/config.properties
+	sed -i "s/\$LICENSE_SERVER_HOST/$LICENSE_SERVER_HOST/g" $CATALINA_HOME/ctp/webapps/em/license
+	sed -i "s/\$LICENSE_SERVER_PORT/$LICENSE_SERVER_PORT/g" $CATALINA_HOME/ctp/webapps/em/license
+fi
 
 echo "Starting the Continuous Testing Platform..."
 mkdir -p $SOAVIRT_HOME/workspace/VirtualAssets/logs/ctp
@@ -36,9 +36,25 @@ then
 	$DATA_REPOSITORY_HOME/server.sh start
 fi
 
-echo "Starting the SOAVirt server..."
-mkdir -p $SOAVIRT_HOME/workspace/VirtualAssets/logs/virtualize
-export CATALINA_BASE=$CATALINA_HOME/soavirt
-nohup $CATALINA_HOME/bin/catalina.sh run >> $SOAVIRT_HOME/workspace/VirtualAssets/logs/virtualize/catalina.log 2>&1 &
+if [ -f $SOAVIRT_HOME/WEB-INF/config.properties ]
+then
+	echo "Starting the SOAVirt server..."
+	mkdir -p $SOAVIRT_HOME/workspace/VirtualAssets/logs/virtualize
+	export CATALINA_BASE=$CATALINA_HOME/soavirt
+	nohup $CATALINA_HOME/bin/catalina.sh run >> $SOAVIRT_HOME/workspace/VirtualAssets/logs/virtualize/catalina.log 2>&1 &
+	sleep 5
+	# Wait for the user to press Ctrl-C to initiate shutdown
+	tail -f $SOAVIRT_HOME/workspace/VirtualAssets/logs/virtualize/catalina.log
+	export CATALINA_BASE=$CATALINA_HOME/soavirt
+	$CATALINA_HOME/bin/catalina.sh stop
+	sleep 5
+	tail $SOAVIRT_HOME/workspace/VirtualAssets/logs/virtualize/catalina.log
+else
+	# Wait for the user to press Ctrl-C to initiate shutdown
+	tail -f $SOAVIRT_HOME/workspace/VirtualAssets/logs/ctp/catalina.log
+fi
 
-tail -f $SOAVIRT_HOME/workspace/VirtualAssets/logs/virtualize/catalina.log
+export CATALINA_BASE=$CATALINA_HOME/ctp
+$CATALINA_HOME/bin/catalina.sh stop
+sleep 5
+tail $SOAVIRT_HOME/workspace/VirtualAssets/logs/ctp/catalina.log
