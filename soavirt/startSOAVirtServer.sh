79777089d03694b41655aee5c722c160d7802f54
Matt Love <mlove@parasoft.com>
2019-03-15 18:00:35 -0700

    env vars for changing virtualize ports on the fly

--- soavirt/startSOAVirtServer.sh
+++ soavirt/startSOAVirtServer.sh
@@ -13,6 +13,8 @@ fi
 # Evaluate and replace the environment variables in the Virtualize config
 
 sed -i "s/\$VIRTUALIZE_SERVER_NAME/$VIRTUALIZE_SERVER_NAME/g" $SOAVIRT_HOME/WEB-INF/config.properties
+sed -i "s/\$VIRTUALIZE_SERVER_PORT/$VIRTUALIZE_SERVER_PORT/g" $SOAVIRT_HOME/WEB-INF/config.properties
+sed -i "s/\$VIRTUALIZE_SERVER_SECURE_PORT/$VIRTUALIZE_SERVER_SECURE_PORT/g" $SOAVIRT_HOME/WEB-INF/config.properties
 sed -i "s/\$CTP_HOST/$CTP_HOST/g" $SOAVIRT_HOME/WEB-INF/config.properties
 sed -i "s/\$CTP_PORT/$CTP_PORT/g" $SOAVIRT_HOME/WEB-INF/config.properties
 sed -i "s/\$CTP_USERNAME/$CTP_USERNAME/g" $SOAVIRT_HOME/WEB-INF/config.properties
